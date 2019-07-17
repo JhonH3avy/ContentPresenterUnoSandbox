b7c977ef8af7450e83b14563030fb1a5a6240b98
Jhon Contreras <jhon.contreras@hubse.com>
2019-07-17 11:08:01 -0500

    Workaround for resources not being loaded in runtime

--- UnoPlatformSandbox/UnoPlatformSandbox.Shared/Controls/BusyIndicator.cs
+++ UnoPlatformSandbox/UnoPlatformSandbox.Shared/Controls/BusyIndicator.cs
@@ -1,19 +1,98 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Animation;
+using Microsoft.Toolkit.Uwp.UI.Extensions;
 
 namespace UnoPlatformSandbox.Shared.Controls
 {
     public class BusyIndicator : ContentControl
     {
+        DoubleAnimation rotationAnimation;
+        Storyboard storyboard;
+        Transform rotateTransform;
+
         protected override void OnApplyTemplate()
         {
             base.OnApplyTemplate();
-            //if (!(GetTemplateChild("root") is Grid rootGrid)) return;
-            //rootGrid.DataContext = this;
-            //var std = rootGrid.Children[0] as ContentPresenter;
-            //std.Content = Content;
+            CraeteStoryboard();
+            if (IsBusy)
+            {
+                StartBusyStoryboard();
+            }
+            else
+            {
+                StopBusyStoryboard();
+            }
         }
+
+        private void CraeteStoryboard()
+        {
+            var rootGrid = GetTemplateChild("root") as Grid;
+            if (rootGrid == null) return;
+            rootGrid.DataContext = this;
+            var visualGrid = rootGrid.FindChildByName("visual") as Grid;
+            visualGrid.RenderTransform = new RotateTransform();
+            storyboard = new Storyboard();
+            rotationAnimation = new DoubleAnimation();
+            rotationAnimation.From = 0;
+            rotationAnimation.To = 360;
+            rotationAnimation.Duration = new Duration(TimeSpan.FromSeconds(5));
+            rotationAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            Storyboard.SetTarget(rotationAnimation, visualGrid);
+            Storyboard.SetTargetProperty(rotationAnimation, "(Grid.RenderTransform).(RotateTransform.Angle)");
+            storyboard.Children.Add(rotationAnimation);
+            //Storyboard.SetTargetProperty(rotationAnimation, "Angle");
+            //Storyboard.SetTargetName(rotationAnimation, "angleTransform");
+        }
+
+        public void StartBusyStoryboard()
+        {
+            //Console.WriteLine("Starting BusyIndicator animation");
+            //var rootGrid = GetTemplateChild("root") as Grid;
+            //if (rootGrid == null)
+            //{
+            //    Console.WriteLine("Cannot find root grid");
+            //    return;
+            //}
+            //Console.WriteLine("Found root grid");
+            //rootGrid.DataContext = this;
+            //var std = rootGrid.Resources["anim"] as Storyboard;
+            //Console.WriteLine($"There are {rootGrid.Resources.Count} resources in rootGrid");
+            //if (std == null)
+            //{
+            //    Console.WriteLine("Cannot find storyboard");
+            //    return;
+            //}
+            //Console.WriteLine("Found storyboard");
+            //std.Begin();
+            storyboard.Begin();
+        }
+
+        public void StopBusyStoryboard()
+        {
+            //Console.WriteLine("Stopping BusyIndicator animation");
+            //var rootGrid = GetTemplateChild("root") as Grid;
+            //if (rootGrid == null)
+            //{
+            //    Console.WriteLine("Cannot find root grid");
+            //    return;
+            //}
+            //Console.WriteLine("Found root grid");
+            //rootGrid.DataContext = this;
+            //var std = rootGrid.Resources["anim"] as Storyboard;
+            //Console.WriteLine($"There are {rootGrid.Resources.Count} resources in rootGrid");
+            //if (std == null)
+            //{
+            //    Console.WriteLine("Cannot find storyboard");
+            //    return;
+            //}
+            //Console.WriteLine("Found storyboard");
+            //std.Stop();
+            storyboard.Stop();
+        }
+
 
         public BusyIndicator()
         {
@@ -29,7 +108,19 @@ namespace UnoPlatformSandbox.Shared.Controls
             }
         }
 
-        public static DependencyProperty IsBusyProperty = DependencyProperty.Register("IsBusy", typeof(bool), typeof(BusyIndicator), new PropertyMetadata(true, null));
+        public static DependencyProperty IsBusyProperty = DependencyProperty.Register("IsBusy", typeof(bool), typeof(BusyIndicator), new PropertyMetadata(true, IsBusyPropertyChanged));
 
+        private static void IsBusyPropertyChanged(DependencyObject dependencyobject, DependencyPropertyChangedEventArgs args)
+        {
+            var busyIndicator = dependencyobject as BusyIndicator;
+            if ((bool)args.NewValue)
+            {
+                busyIndicator.StartBusyStoryboard();
+            }
+            else
+            {
+                busyIndicator.StopBusyStoryboard();
+            }
+        }
     }
 }
